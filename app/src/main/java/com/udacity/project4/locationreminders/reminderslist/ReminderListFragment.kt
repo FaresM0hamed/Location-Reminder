47bad1eddf01f281110c8c9e65f22376953ca419
Fares <faresmohamed123321@gmail.com>
2023-01-07 16:23:53 +0200

    starter project initial commit

---
+++ app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -0,0 +1,89 @@
+package com.udacity.project4.locationreminders.reminderslist
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.udacity.project4.R
+import com.udacity.project4.base.BaseFragment
+import com.udacity.project4.base.BaseViewModel
+import com.udacity.project4.base.NavigationCommand
+import com.udacity.project4.databinding.FragmentRemindersBinding
+import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
+import com.udacity.project4.utils.setTitle
+import com.udacity.project4.utils.setup
+
+class ReminderListFragment(override val _viewModel: BaseViewModel) : BaseFragment() {
+    //use Koin to retrieve the ViewModel instance
+    private val viewModel by viewModels<RemindersListViewModel>()
+    private lateinit var binding: FragmentRemindersBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_reminders, container, false
+            )
+        binding.viewModel = viewModel
+
+        setHasOptionsMenu(true)
+        setDisplayHomeAsUpEnabled(false)
+        setTitle(getString(R.string.app_name))
+
+        binding.refreshLayout.setOnRefreshListener { viewModel.loadReminders() }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        setupRecyclerView()
+        binding.addReminderFAB.setOnClickListener {
+            navigateToAddReminder()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //load the reminders list on the ui
+        viewModel.loadReminders()
+    }
+
+    private fun navigateToAddReminder() {
+        //use the navigationCommand live data to navigate between the fragments
+        viewModel.navigationCommand.postValue(
+            NavigationCommand.To(
+                ReminderListFragmentDirections.toSaveReminder()
+            )
+        )
+    }
+
+    private fun setupRecyclerView() {
+        val adapter = RemindersListAdapter {
+        }
+
+//        setup the recycler view using the extension function
+        binding.reminderssRecyclerView.setup(adapter)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+//                TODO: add the logout implementation
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+//        display logout as menu item
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+}
---
+++ app/src/test/java/com/udacity/project4/savereminder/SaveReminderViewModelTest.kt
@@ -0,0 +1,15 @@
+package com.udacity.project4.savereminder
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class SaveReminderViewModelTest {
+
+
+    //TODO: provide testing to the SaveReminderView and its live data objects
+
+
+}
